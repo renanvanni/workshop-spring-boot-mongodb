541c69194be182cce6fd6b12a4d5d046a6dde4d3
STP-User-08 <renanvanni13@gmail.com>
2022-09-29 18:26:07 -0300

    Atualização de usuário com PUT

--- src/main/java/com/renanvanni/workshopmongo/services/UserService.java
+++ src/main/java/com/renanvanni/workshopmongo/services/UserService.java
@@ -35,6 +35,17 @@ public class UserService {
 		userRepository.deleteById(id);
 	}
 	
+	public User update(User user) {
+		User obj = findById(user.getId());
+		updateData(obj, user);
+		return userRepository.save(obj);
+	}
+	
+	private void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+	}
+	
 	public User fromDTO(UserDTO userDTO) {
 		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
