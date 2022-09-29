34d19e9aad577bc7bb8c2b39c455ffe8c8be2c8b
STP-User-08 <renanvanni13@gmail.com>
2022-09-29 15:59:27 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/renanvanni/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renanvanni.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
